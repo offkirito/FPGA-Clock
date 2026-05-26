5f17e2a73f3fbdc0bdd0be50c3b8a4fcd9192105
Jair Moshia Montoya Enriquez <mosh09crack@gmail.com>
2026-05-26 08:47:57 -0700

    Add files via upload

---
+++ clockss.v
@@ -0,0 +1,45 @@
+module clockss
+(
+	
+	
+);
+
+reg [31:0] clk_divider; //contador 32-bit para dividir los 50Mhz a 1Hz.
+
+reg slow_clk; // clk 1Hz
+
+
+
+always @ (posedge clk or posedge reset) begin
+
+  if (reset) begin
+
+    clk_divider <= 32'd0;
+
+    slow_clk <= 0;
+
+  end else begin
+
+    if (clk_divider == 32'd25000000) begin // Cambiar de estado cada 25M ciclos
+
+      slow_clk <= ~slow_clk;
+
+      clk_divider <= 32'd0;
+
+    end else begin
+
+      clk_divider <= clk_divider + 1;
+
+    end
+
+  end
+
+end
+
+
+
+
+
+ 
+
+ always @ (posedge slow_clk or posedge reset) begin
