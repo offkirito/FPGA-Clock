f5900781d3e1299f2db4822db5f300fc37b4e7e5
Jair Moshia Montoya Enriquez <mosh09crack@gmail.com>
2026-06-04 12:01:41 -0700

    Refactor clock module for clarity and organization
    Refactor clock module for improved readability and maintainability.

--- Finalclock.v
+++ Finalclock.v
@@ -1,77 +1,73 @@
 module clockss
 (
-    input clk,          // Reloj principal (50MHz)
-    input reset,        // Botón de reinicio (Activo en bajo / 0)
-    input pause,        // Interruptor de pausa
-    output [3:0] dig_sel, // Activa qué pantalla encender (Dígitos 1 al 4)
-    output [6:0] seg      // Enciende los LEDs físicos (Segmentos a-g)
+    input clk, reset, pause,        
+    output [3:0] dig_sel, [6:0] seg // dig_sel seleccion el display que se va a prender y seg son los segemntos como (a, b, c, d, e, f, g)
 );
 
-// --- 1. MEMORIA DEL RELOJ ---
-reg [3:0] bcd [0:3];     // bcd[0]=min_u, bcd[1]=min_t, bcd[2]=hr_u, bcd[3]=hr_t
-reg [31:0] clk_divider;  // Cuenta para 1 segundo
-reg [5:0] sec;           // Cuenta hasta 59 segundos
 
-// --- 2. MEMORIA PARA LA PANTALLA (MUX Y DECODIFICADOR) ---
+reg [3:0] bcd [0:3];     // bcd[0]=min_u, bcd[1]=min_t, bcd[2]=hr_u, bcd[3]=hr_t
+reg [31:0] clk_divider;  // Cuenta para un segundo
+reg [5:0] sec;           // Cuenta hasta los 60 segundos para mandar el tick al minutos
+
 reg [16:0] refresh_counter; // Contador rápido para alternar pantallas
 reg [3:0] display_activo;   // Variable temporal para el dig_sel
 reg [3:0] numero_actual;    // Variable que le pasamos al traductor de LEDs
 reg [6:0] leds;             // Variable temporal para los segmentos
 
-// Conexión de variables temporales a los pines físicos de salida
+// Assignacion de las variables temporales a las salidas finales
 assign dig_sel = display_activo;
 assign seg = leds;
 
-// --- 3. BANDERAS DE TIEMPO ---
-wire tick_1hz   = (clk_divider == 32'd99999);     // 1 segundo
-wire tick_1min  = (tick_1hz   && sec == 6'b111011);  // 59 seg
-wire tick_10min = (tick_1min  && bcd[0] == 4'b1001); // 9 min
-wire tick_1hr   = (tick_10min && bcd[1] == 4'b0101); // 50 min
 
-// --- 4. CONTADOR DE SEGUNDOS ---
-// Cambiamos a negedge porque tu botón manda 0 al presionarse
+wire tick_1hz   = (clk_divider == 32'd99999);     // Hace el tick de 1 segundo
+wire tick_1min  = (tick_1hz   && sec == 6'b111011);  // Condicional para hacer un minuto
+wire tick_10min = (tick_1min  && bcd[0] == 4'b1001); // Condicional hecho para separar los digitos decimales para luego mostralos
+    wire tick_1hr   = (tick_10min && bcd[1] == 4'b0101); // Cuenta 50 minutos porque esta seperado cada digito decimal
+
+
+// Contador de segundos
 always @(posedge clk or negedge reset) begin
-    if (!reset) begin // Si el botón manda 0, reinicia
-        clk_divider <= 32'd0;
+    if (!reset) begin // Si el botón manda 0 se reinicia (haciendolo por el push button del fpga)
+        clk_divider <= 32'd0; // cada que se manda un tick de 1hz el contador cl clk_divider
         sec         <= 6'b000000;
-    end else if (!pause) begin
+    end else if (!pause) begin // si el switch de pausa no es 0 sigue el reloj
         if (tick_1hz) begin
             clk_divider <= 32'd0;
             if (sec == 6'b111011) begin
-                sec <= 6'b000000; // Reinicia a 0
-            end else begin
-                sec <= sec + 6'b000001; // Suma 1
+                sec <= 6'b000000;
+            end else begin // contador de los segundos
+                sec <= sec + 6'b000001;
             end
-        end else begin
+        end else begin // contador para un tick de un segundo
             clk_divider <= clk_divider + 32'd1;
         end
     end
 end
 
-// --- 5. CONTADOR DE MINUTOS Y HORAS ---
+// Contador de minutos y horas
 always @(posedge clk or negedge reset) begin
-    if (!reset) begin // Si el botón manda 0, reinicia
+    if (!reset) begin // Si el botón manda 0 se reinicia
         bcd[0] <= 4'b0000;
         bcd[1] <= 4'b0000;
         bcd[2] <= 4'b0000;
         bcd[3] <= 4'b0000;
     end else if (!pause) begin
-        // Unidades de minuto
+        // Cada tick de un minuto hecho por el assign declarados se guarda en el espacio 1 del arreglo,
         if (tick_1min) begin
             if (bcd[0] == 4'b1001) bcd[0] <= 4'b0000;
             else                   bcd[0] <= bcd[0] + 4'b0001;
         end
-        // Decenas de minuto
+        // Tick de 10 minutos para guardar el segundo digito en el display
         if (tick_10min) begin
             if (bcd[1] == 4'b0101) bcd[1] <= 4'b0000;
             else                   bcd[1] <= bcd[1] + 4'b0001;
         end
         // Horas
         if (tick_1hr) begin
-            if (bcd[3] == 4'b0001 && bcd[2] == 4'b0001) begin // Si son las 11
+            if (bcd[3] == 4'b0001 && bcd[2] == 4'b0001) begin // si son las 11 y 59 minutos y segundos se reinicia
                 bcd[2] <= 4'b0000;
                 bcd[3] <= 4'b0000;
-            end else if (bcd[2] == 4'b1001) begin             // Si llega a 9
+            end else if (bcd[2] == 4'b1001) begin
                 bcd[2] <= 4'b0000;
                 bcd[3] <= bcd[3] + 4'b0001;
             end else begin
@@ -81,7 +77,7 @@ always @(posedge clk or negedge reset) begin
     end
 end
 
-// --- 6. VELOCIDAD DEL MULTIPLEXOR ---
+// Hicimos otro delimitador de tiempo ahora de 762 Hz que cuenta 17 bits y no ponemos que se haga 0 y confiamos que simplemente el carry out se pierda en el hardware, ahorrandos lineas
 always @(posedge clk or negedge reset) begin
     if (!reset) begin // Si el botón manda 0, reinicia el barrido
         refresh_counter <= 17'd0;
@@ -90,7 +86,7 @@ always @(posedge clk or negedge reset) begin
     end
 end
 
-// Extraemos los bits más altos para que cuente 00, 01, 10, 11 lentamente
+// olo buscamos los 2 bits mas alto para de limitar el contador del display
 wire [1:0] selector = refresh_counter[16:15];
 
 // Creamos esto dos wire para hacer que el front end cambie dependiendo las variables de bcd
@@ -101,30 +97,29 @@ wire [3:0] dec_h;
     assign uni_h = (bcd[2] == 4'b0 && bcd[3] == 4'b0) ? 4'b0010 : bcd[2];
     assign dec_h = (bcd[2] == 4'b0 && bcd[3] == 4'b0) ? 4'b0001 : bcd[3];
     
-// --- 7. MULTIPLEXOR (Alterna las pantallas) ---
-// Lógica activa en BAJO (0 enciende la pantalla, 1 la apaga)
+// Multiplexor de los digitos
+// Logica activa en BAJO, teniendo 4 ditios entonces siendo
 always @(*) begin
     if (selector == 2'b00) begin
-        display_activo = 4'b1110; // Enciende Dígito 1 (Derecha)
-        numero_actual  = bcd[0];  // Manda Unidades de Minuto
+        display_activo = 4'b1110; // Dsiplay 1 manda el primer digito de minutos
+        numero_actual  = bcd[0];  
     end 
     else if (selector == 2'b01) begin
-        display_activo = 4'b1101; // Enciende Dígito 2
-        numero_actual  = bcd[1];  // Manda Decenas de Minuto
+        display_activo = 4'b1101; // Dsiplay 2 manda el primer digito de minutos
+        numero_actual  = bcd[1];  
     end 
     else if (selector == 2'b10) begin
-        display_activo = 4'b1011; // Enciende Dígito 3
-        numero_actual  = uni_h;  // Manda Unidades de Hora
+        display_activo = 4'b1011; // Dsiplay 1 manda el primer digito de horas
+        numero_actual  = uni_h;  
     end 
     else begin // 2'b11
-        display_activo = 4'b0111; // Enciende Dígito 4 (Izquierda)
-        numero_actual  = dec_h;  // Manda Decenas de Hora
+        display_activo = 4'b0111; // Dsiplay 4 manda el 2 digito de horas
+        numero_actual  = dec_h;  
     end
 end
 
-// --- 8. DECODIFICADOR 7 SEGMENTOS (CORREGIDO) ---
+
 // Orden de los bits: 7'b(g)(f)(e)(d)(c)(b)(a)
-// Lógica POSITIVA (1 enciende el LED, 0 lo apaga)
 always @(*) begin
     if      (numero_actual == 4'b0000) leds = 7'b0111111; // Dibuja 0
     else if (numero_actual == 4'b0001) leds = 7'b0000110; // Dibuja 1
