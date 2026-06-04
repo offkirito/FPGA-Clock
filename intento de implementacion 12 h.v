970084402d38bf53320b63dbd41bfefd5b64e1ae
Jair Moshia Montoya Enriquez <mosh09crack@gmail.com>
2026-06-04 02:13:12 -0700

    Update intento de implementacion 12 h.v

--- intento de implementacion 12 h.v
+++ intento de implementacion 12 h.v
@@ -4,7 +4,7 @@ wire [3:0] current_bcd;
 wire [3:0] uni_h
 wire [3:0] dec_h
 
-// Si uni_h/dec_h son 0 se les asigna 1 y 2 respectivamente, else se les asigna el valor que se tienen guaradado en bcd
+// Si uni_h y dec_h son 0 se les asigna 1 y 2 respectivamente, else se les asigna el valor que se tienen guaradado en bcd
 assign uni_h = (bcd[2] == 4'b0 && bcd[3] == 4'b0) ? 4'b1 : bcd[2];
 assign dec_h = (bcd[2] == 4'b0 && bcd[3] == 4'b0) ? 4'b2 : bcd[3];
 
